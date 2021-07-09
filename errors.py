c72f367c44fe15ef07f3638269733a35e0818181
PeterKirillow <34065219+PeterKirillow@users.noreply.github.com>
2021-07-09 11:29:27 +0300

    Create errors.py

---
+++ errors.py
@@ -0,0 +1,14 @@
+{
+    "Code": 0,
+    "Message": "",
+    "Exception": null,
+    "PasswordChangeUrl": null,
+    "RedirectUrl": null
+}
+
+{
+    "error": {
+        "code": "",
+        "message": "An error has occurred."
+    }
+}
